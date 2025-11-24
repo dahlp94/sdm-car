30b7dbe459987ccab5a233a0250f71976dba3b63
Pratik Dahal <mapratikdahal@gmail.com>
2025-11-24 16:59:24 -0600

    modified init file

--- sdmcar/__init__.py
+++ sdmcar/__init__.py
@@ -1,7 +1,7 @@
 # sdmcar/__init__.py
 
 from .graph import build_laplacian_from_knn, laplacian_eigendecomp
-from .filters import DiffusionFilterFullVI
+from .filters import DiffusionFilterFullVI, MaternLikeFilterFullVI
 from .models import SpectralCAR_FullVI
 from . import diagnostics
 from .utils import set_seed, set_default_dtype
@@ -10,6 +10,7 @@ __all__ = [
     "build_laplacian_from_knn",
     "laplacian_eigendecomp",
     "DiffusionFilterFullVI",
+    "MaternLikeFilterFullVI",
     "SpectralCAR_FullVI",
     "diagnostics",
     "set_seed",
