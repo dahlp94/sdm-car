c3a96a1458a8e66a872f6113ddf30539d421a74d
Pratik Dahal <mapratikdahal@gmail.com>
2026-02-06 00:22:18 -0600

    enable automatic benchmark registration on import

---
+++ examples/benchmarks/__init__.py
@@ -0,0 +1,4 @@
+# examples/benchmarks/__init__.py
+# Importing these registers the filters/cases into the registry.
+from . import matern      # noqa: F401
+from . import invlinear_car  # noqa: F401
