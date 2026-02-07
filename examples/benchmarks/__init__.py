92aa96a4696c8149c0aa6aa8e0657fe99530c844
Pratik Dahal <mapratikdahal@gmail.com>
2026-02-06 19:03:00 -0600

    Add polyrational to the registry

--- examples/benchmarks/__init__.py
+++ examples/benchmarks/__init__.py
@@ -2,3 +2,4 @@
 # Importing these registers the filters/cases into the registry.
 from . import matern      # noqa: F401
 from . import invlinear_car  # noqa: F401
+from . import polyrational  # noqa: F401
