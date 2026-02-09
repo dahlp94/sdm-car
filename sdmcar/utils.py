8e569878a6a80915311a372bc9123a7b3a067654
Pratik Dahal <mapratikdahal@gmail.com>
2026-02-09 16:18:49 -0600

    Add KL against a general Normal prior

--- sdmcar/utils.py
+++ sdmcar/utils.py
@@ -25,3 +25,16 @@ def kl_normal_std(mu, log_std):
     """
     std = torch.exp(log_std)
     return 0.5 * (mu**2 + std**2 - 1.0 - 2.0 * log_std)
+
+def kl_normal_to_normal(mu_q, log_std_q, mu_p: float, std_p: float):
+    """
+    KL( N(mu_q, std_q^2) || N(mu_p, std_p^2) )
+    mu_q, log_std_q are tensors (shape [1] ok).
+    mu_p, std_p are floats (or tensors).
+    """
+    std_q = torch.exp(log_std_q)
+    mu_p_t = torch.tensor([mu_p], dtype=mu_q.dtype, device=mu_q.device)
+    std_p_t = torch.tensor([std_p], dtype=mu_q.dtype, device=mu_q.device)
+
+    kl = torch.log(std_p_t / std_q) + (std_q**2 + (mu_q - mu_p_t)**2) / (2.0 * std_p_t**2) - 0.5
+    return kl
