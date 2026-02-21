eb801f1e7d552ceeab50319ad5096a56ff8a5bcf
Pratik Dahal <mapratikdahal@gmail.com>
2026-02-21 17:56:52 -0600

    Register logspline benchmark module in filter registry

--- examples/benchmarks/__init__.py
+++ examples/benchmarks/__init__.py
@@ -1,7 +1,8 @@
 # examples/benchmarks/__init__.py
 # Importing these registers the filters/cases into the registry.
-from . import matern      # noqa: F401
-from . import invlinear_car  # noqa: F401
-from . import polyrational  # noqa: F401
-from . import classic_car  # noqa: F401
-from . import leroux  # noqa: F401
+from . import matern
+from . import invlinear_car
+from . import polyrational
+from . import classic_car
+from . import leroux
+from . import logspline
