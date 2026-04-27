8e38038ace7abc38928b819f5eb47e1fe4d516a1
Pratik Dahal <mapratikdahal@gmail.com>
2026-04-26 23:26:58 -0500

    Expose new spectral filters in package init for registry access

--- sdmcar/__init__.py
+++ sdmcar/__init__.py
@@ -1,7 +1,7 @@
 # sdmcar/__init__.py
 
 from .graph import build_laplacian_from_knn, laplacian_eigendecomp
-from .filters import DiffusionFilterFullVI, MaternLikeFilterFullVI
+from .filters import DiffusionKernelFilterFullVI, MaternLikeFilterFullVI
 from .models import SpectralCAR_FullVI
 from . import diagnostics
 from .utils import set_seed, set_default_dtype
@@ -9,7 +9,7 @@ from .utils import set_seed, set_default_dtype
 __all__ = [
     "build_laplacian_from_knn",
     "laplacian_eigendecomp",
-    "DiffusionFilterFullVI",
+    "DiffusionKernelFilterFullVI",
     "MaternLikeFilterFullVI",
     "SpectralCAR_FullVI",
     "diagnostics",
